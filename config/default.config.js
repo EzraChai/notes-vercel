0bf17df0c759059f7912a2a39538b2c87e3fc10a
ezrachai <juanzhe2@gmail.com>
2021-08-22 15:12:57 +0800

    bugs fixed

--- config/default.config.js
+++ config/default.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    dbURL : process.env.DB_URL,
-    secretKey : process.env.SECRET_KEY,
-    jwtSecret : process.env.JWT_SECRET_KEY,
+    dbURL : 'mongodb+srv://ezrachai:Ysvrp4klglooGOdK@cluster0.nnxre.mongodb.net/realworld?retryWrites=true&w=majority', 
+    secretKey : '3338be694SDf5y2dok50c5',
+    jwtSecret : '3bb641ba-d64b-4f21-9f6e-48eaf022ade6',
 }
