2504d467245e8653243556c310635883b5ba3d64
ezrachai <juanzhe2@gmail.com>
2021-08-22 15:20:30 +0800

    bugs fixed

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const router = require('./router')
+const router = require('./api')
 const morgan = require('morgan');
 const cors = require('cors');
 const ErrorHandler = require('./middleware/error-handler')
